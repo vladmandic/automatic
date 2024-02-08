235bd18516ef0c1784367d48f497071ca403cdc7
Seunghoon Lee <lshqqytiger@naver.com>
2024-02-09 03:43:46 +0900

    onnx tooltip, olive-ai check

--- modules/onnx_impl/ui.py
+++ modules/onnx_impl/ui.py
@@ -17,6 +17,7 @@ def create_ui():
     from modules.shared import log, opts, cmd_opts, refresh_checkpoints
     from modules.sd_models import checkpoint_tiles, get_closet_checkpoint_match
     from modules.paths import sd_configs_path
+    from . import run_olive_workflow
     from .execution_providers import ExecutionProvider, install_execution_provider
     from .utils import check_diffusers_cache
 
@@ -47,7 +48,7 @@ def create_ui():
 
                     ep_install.click(fn=install_execution_provider, inputs=ep_checkbox)
 
-            if opts.cuda_compile_backend == "olive-ai":
+            if run_olive_workflow is not None:
                 import olive.passes as olive_passes
                 from olive.hardware.accelerator import AcceleratorSpec, Device
 
