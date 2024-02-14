25c652782fa76b1cad9e8b1f509c96422d1a62b0
Seunghoon Lee <lshqqytiger@naver.com>
2024-02-15 07:40:16 +0900

    for Windows only

--- modules/zluda.py
+++ modules/zluda.py
@@ -1,8 +1,9 @@
+import platform
 import torch
 from modules import shared, devices
 
 
 def initialize_zluda():
-    if devices.cuda_ok and torch.cuda.get_device_name(devices.get_optimal_device()).endswith("[ZLUDA]"):
+    if platform.system() == "Windows" and devices.cuda_ok and torch.cuda.get_device_name(devices.get_optimal_device()).endswith("[ZLUDA]"):
         torch.backends.cudnn.enabled = False
         shared.opts.cross_attention_optimization = "Batch matrix-matrix"
