cc9d0960c2e7a2d5f2ede52ede95635a65f37598
Disty0 <semihgulec2005@gmail.com>
2024-02-15 20:42:34 +0300

    ZLUDA loosen attention settings

--- modules/zluda.py
+++ modules/zluda.py
@@ -6,4 +6,5 @@ from modules import shared, devices
 def initialize_zluda():
     if platform.system() == "Windows" and devices.cuda_ok and torch.cuda.get_device_name(devices.get_optimal_device()).endswith("[ZLUDA]"):
         torch.backends.cudnn.enabled = False
-        shared.opts.cross_attention_optimization = "Batch matrix-matrix"
+        if shared.opts.cross_attention_optimization == "Scaled-Dot-Product":
+            shared.opts.cross_attention_optimization = "Batch matrix-matrix"
