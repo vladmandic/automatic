0e1fb9c18a3cdc70d2bc4a5233ca029a98da985e
Seunghoon Lee <lshqqytiger@naver.com>
2024-02-20 00:00:06 +0900

    zluda force sdp_options to math

--- modules/zluda.py
+++ modules/zluda.py
@@ -12,3 +12,4 @@ def initialize_zluda():
         torch.backends.cuda.enable_flash_sdp(False)
         torch.backends.cuda.enable_math_sdp(True)
         torch.backends.cuda.enable_mem_efficient_sdp(False)
+        shared.opts.sdp_options = ['Math attention']
