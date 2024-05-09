72c4970742667f55281bec9d2639a9971472ff23
Vladimir Mandic <mandic00@live.com>
2024-05-09 08:54:59 -0400

    fix model detect

--- modules/control/units/detect.py
+++ modules/control/units/detect.py
@@ -1,10 +1,13 @@
 import diffusers.pipelines as p
 
+
 def is_sd15(model):
     if model is None:
         return False
-    return model.__name__ == p.StableDiffusionPipeline.__name__ or model.__name__ == p.StableDiffusionImg2ImgPipeline.__name__ or model.__name__ == p.StableDiffusionInpaintPipeline.__name__
-    # return isinstance(model, p.StableDiffusionPipeline) or isinstance(model, p.StableDiffusionImg2ImgPipeline) or isinstance(model, p.StableDiffusionInpaintPipeline)
+    if hasattr(model, '__name__'):
+        return model.__name__ == p.StableDiffusionPipeline.__name__ or model.__name__ == p.StableDiffusionImg2ImgPipeline.__name__ or model.__name__ == p.StableDiffusionInpaintPipeline.__name__
+    return isinstance(model, p.StableDiffusionPipeline) or isinstance(model, p.StableDiffusionImg2ImgPipeline) or isinstance(model, p.StableDiffusionInpaintPipeline)
+
 
 def is_sdxl(model):
     if model is None:
