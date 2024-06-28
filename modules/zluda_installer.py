3fc0072911ced8817bc01796d55f95717050dffe
Seunghoon Lee <lshqqytiger@naver.com>
2024-06-28 21:39:01 +0900

    revert zluda installer

--- modules/zluda_installer.py
+++ modules/zluda_installer.py
@@ -3,7 +3,7 @@ import ctypes
 import shutil
 import zipfile
 import platform
-import requests
+import urllib.request
 from typing import Union
 
 
@@ -36,10 +36,7 @@ def install(zluda_path: os.PathLike) -> None:
     if platform.system() != 'Windows': # Windows-only. (PyTorch should be rebuilt on Linux)
         return
 
-    with open('_zluda', 'wb') as file:
-        res = requests.get(f'https://github.com/lshqqytiger/ZLUDA/releases/download/{RELEASE}/ZLUDA-windows-amd64.zip', timeout=30)
-        file.write(res.content)
-
+    urllib.request.urlretrieve(f'https://github.com/lshqqytiger/ZLUDA/releases/download/{RELEASE}/ZLUDA-windows-amd64.zip', '_zluda')
     with zipfile.ZipFile('_zluda', 'r') as archive:
         infos = archive.infolist()
         for info in infos:
