74ed1644f695cd4bdb7d60bba3126572f398ed61
Yoink3000 <hugongai98@gmail.com>
2024-07-01 22:57:47 +0800

    assuming cuda if no compute platform specified

--- container.bat
+++ container.bat
@@ -1,18 +1,21 @@
 @echo off
+cd /d "%~dp0"
 
-if "%1"=="--cuda" GOTO build_and_run
-if "%1"=="--rocm" GOTO build_and_run
-if "%1"=="--cpu" GOTO build_and_run
+if "%1"=="--cuda" GOTO parseFlag
+if "%1"=="--rocm" GOTO parseFlag
+if "%1"=="--cpu" GOTO parseFlag
 
+echo Compute platform not specified, assuming compute platform is CUDA
+echo.
+set COMPUTE=cuda
+GOTO buildAndRun
 
-echo Please run with one of the following flags:
-echo --cuda, --rocm, --cpu
-exit /b 1
-
-:build_and_run
+:parseFlag
 set COMPUTE=%~1
 set COMPUTE=%COMPUTE:--=%
+GOTO buildAndRun
 
+:buildAndRun
 docker build -t sd-next -f ./docker/%COMPUTE%.Dockerfile .
 docker rm "SD-Next"
 docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python/venv -v SD-Next_Cache:/root/.cache --group-add video -p 7860:7860 --gpus=all --name "SD-Next" sd-next
