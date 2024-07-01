164e35b2e77bd74c815a2bf30b0cb3d606974151
Yoink3000 <hugongai98@gmail.com>
2024-07-01 23:20:10 +0800

    change volume mount path, leave venv for sd next

--- container.sh
+++ container.sh
@@ -12,4 +12,4 @@ fi
 
 docker build -t sd-next -f ./docker/$COMPUTE.Dockerfile .
 docker rm "SD-Next"
-docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python/venv -v SD-Next_Cache:/root/.cache --group-add video -p 7860:7860 --gpus=all --name "SD-Next" sd-next
+docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python -v SD-Next_Cache:/root/.cache -p 7860:7860 --gpus=all --name "SD-Next" sd-next
