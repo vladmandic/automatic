b950c777c55ada2df17f1a602c0bfc8f5cffe778
Yoink3000 <hugongai98@gmail.com>
2024-07-01 22:57:47 +0800

    assuming cuda if no compute platform specified

--- container.sh
+++ container.sh
@@ -5,12 +5,11 @@ cd -- "$(dirname -- "$0")"
 if [ "$1" = "--cuda" ] || [ "$1" = "--rocm" ] || [ "$1" = "--cpu" ]; then
     COMPUTE=${1#--}
     echo "COMPUTE: $COMPUTE"
-
-    docker build -t sd-next -f ./docker/$COMPUTE.Dockerfile .
-    docker rm "SD-Next"
-    docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python/venv -v SD-Next_Cache:/root/.cache --group-add video -p 7860:7860 --gpus=all --name "SD-Next" sd-next
 else
-    echo "Please run with one of the following flags:"
-    echo "--cuda, --rocm, --cpu"
-    exit 1
+    echo "Compute platform not specified, assuming compute platform is CUDA"
+    COMPUTE=cuda
 fi
+
+docker build -t sd-next -f ./docker/$COMPUTE.Dockerfile .
+docker rm "SD-Next"
+docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python/venv -v SD-Next_Cache:/root/.cache --group-add video -p 7860:7860 --gpus=all --name "SD-Next" sd-next
