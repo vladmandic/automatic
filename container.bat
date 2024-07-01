f10aca5306ae6d51d09b8bb92aac3eca174b6ba2
Yoink3000 <hugongai98@gmail.com>
2024-07-01 21:18:51 +0800

    build with Dockerfile directly instead of compose

---
+++ container.bat
@@ -0,0 +1,19 @@
+@echo off
+
+if not defined PYTHON (set PYTHON=python)
+if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
+
+%PYTHON% -m venv "%VENV_DIR%"
+
+set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+
+for /f "usebackq delims=" %%i in (`%PYTHON% ./docker/docker.py %*`) do (
+    echo %%i
+    set "img=%%i"
+)
+
+echo.
+
+docker build -t sd-next -f ./docker/Dockerfile --build-arg "BASE_IMG=%img%" .
+docker rm "SD-Next"
+docker run -it --device /dev/dri -v SD-Next:/workspace -v SD-Next_Venv:/python/venv -v SD-Next_Cache:/root/.cache -p 7860:7860 --gpus=all --name "SD-Next" sd-next
