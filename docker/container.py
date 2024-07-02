8365a219d5785606a6697074476c61ad5d3fd684
Yoink3000 <hugongai98@gmail.com>
2024-07-03 04:15:47 +0800

    update docker args for different compute

--- docker/container.py
+++ docker/container.py
@@ -6,20 +6,23 @@ compute, noV, V, port, name = args.compute, args.no_volume, args.volumes, args.p
 
 dockerArgs = ["-it", f"-p {port}:7860", f"--name {name}"]
 
-if compute != "cpu": dockerArgs.append("--gpus=all --device /dev/dri")
+if compute == "cuda": dockerArgs.append("--gpus all")
+if compute == "rocm": dockerArgs.append("--device /dev/dri --device=/dev/kfd")
 
 if not noV:
     for vName, vPath in V.items():
         dockerArgs.append(f'-v {("SD-Next_" if not vName =="SD-Next" else "")+vName}:{vPath}')
 
+dockerArgs= " ".join(dockerArgs)
 print(f'''
 Container Settings:
     Name: {name}
     Port: {port}
     Compute Platform: {compute}
     Volumes: {"Disabled" if noV else ", ".join([f'{key} -> {value}' for key, value in V.items()])}
+    Args: {dockerArgs}
 ''')
 cmdStream(f"docker container rm {name} -f", msg=f"Removing container named {name}...", expectErr=True)
 cmdStream(f"docker image rm sd-next -f", msg="Removing image named sd-next...", expectErr=True)
 cmdStream(f"docker build -t sd-next -f ./{compute}.Dockerfile ../", msg="Building Docker Image (might takes few minutes)...")
-cmdStream(f'docker run {" ".join(dockerArgs)} sd-next')
+cmdStream(f'docker run {dockerArgs} sd-next')
