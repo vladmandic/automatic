87d41886a5c253a18f1cb28a2738d7e23593545f
Yoink3000 <hugongai98@gmail.com>
2024-07-04 03:33:17 +0800

    update packages install with setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -8,10 +8,7 @@ apt upgrade -y > /dev/null
 
 echo Installing apt packages...
 apt install -y git \
-    python3.10 \
-    pythonpy \
-    python3.10-venv \
-    python3-pip \
-    curl \
-    wget \
-    aria2 > /dev/null
+    python3.10 pythonpy python3.10-venv python3-pip \
+    curl wget aria2 \
+    libcairo2 libcairo2-dev libgl1 libglib2.0-0 libgoogle-perftools4 libtcmalloc-minimal4 \
+    ffmpeg pkg-config > /dev/null
