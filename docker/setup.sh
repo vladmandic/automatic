4ea3f35acca8bc26e9fec1f15b2c78dc65f3db34
Yoink3000 <hugongai98@gmail.com>
2024-08-09 00:17:49 +0800

    remove apt packages

--- docker/setup.sh
+++ docker/setup.sh
@@ -23,6 +23,4 @@ echo =========================
 echo
 apt-fast install -y git \
     python3.10 pythonpy python3.10-venv python3-pip \
-    curl wget aria2 \
-    libcairo2 libcairo2-dev libgl1 libglib2.0-0 libgoogle-perftools4 libtcmalloc-minimal4 \
-    ffmpeg pkg-config
+    curl wget aria2
