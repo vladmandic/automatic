e1c40384ad59c16e7f8f33790aada5e74474d206
Seunghoon Lee <lshqqytiger@naver.com>
2024-08-09 01:11:13 +0900

    zluda hijack torch jit

--- modules/zluda_hijacks.py
+++ modules/zluda_hijacks.py
@@ -8,6 +8,12 @@ def topk(tensor: torch.Tensor, *args, **kwargs):
     return torch.return_types.topk((values.to(device), indices.to(device),))
 
 
+def jit_script(f, *_, **__): # experiment / provide dummy graph
+    f.graph = torch._C.Graph() # pylint: disable=protected-access
+    return f
+
+
 def do_hijack():
     torch.version.hip = "5.7"
     torch.topk = topk
+    torch.jit.script = jit_script
