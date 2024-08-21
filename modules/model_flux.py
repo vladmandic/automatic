3165bbc4f8080159807706270831ab97327ed45d
Disty0 <disty@disty.xyz>
2024-08-22 01:29:35 +0300

    Flux quant loding detect dtype from state_dict

--- modules/model_flux.py
+++ modules/model_flux.py
@@ -5,7 +5,7 @@ import transformers
 from safetensors.torch import load_file
 from huggingface_hub import hf_hub_download
 
-from modules import shared
+from modules import shared, devices
 
 
 
@@ -13,9 +13,10 @@ def load_quanto_transformer(repo_path):
     from optimum.quanto import requantize
     with open(repo_path + "/" + "transformer/quantization_map.json", "r") as f:
         quantization_map = json.load(f)
-    with torch.device("meta"):
-        transformer = diffusers.FluxTransformer2DModel.from_config(repo_path + "/" + "transformer/config.json").to(torch.bfloat16)
     state_dict = load_file(repo_path + "/" + "transformer/diffusion_pytorch_model.safetensors")
+    dtype = state_dict['context_embedder.bias'].dtype
+    with torch.device("meta"):
+        transformer = diffusers.FluxTransformer2DModel.from_config(repo_path + "/" + "transformer/config.json").to(dtype=dtype)
     requantize(transformer, state_dict, quantization_map, device=torch.device("cpu"))
     transformer.eval()
     return transformer
@@ -27,14 +28,18 @@ def load_quanto_text_encoder_2(repo_path):
         quantization_map = json.load(f)
     with open(repo_path + "/" + "text_encoder_2/config.json") as f:
         t5_config = transformers.T5Config(**json.load(f))
-    with torch.device("meta"):
-        text_encoder_2 = transformers.T5EncoderModel(t5_config).to(torch.bfloat16)
     state_dict = load_file(repo_path + "/" + "text_encoder_2/model.safetensors")
+    dtype = state_dict['encoder.block.0.layer.0.SelfAttention.relative_attention_bias.weight'].dtype
+    with torch.device("meta"):
+        text_encoder_2 = transformers.T5EncoderModel(t5_config).to(dtype=dtype)
     requantize(text_encoder_2, state_dict, quantization_map, device=torch.device("cpu"))
     text_encoder_2.eval()
     return text_encoder_2
 
 def load_flux(checkpoint_info, diffusers_load_config):
+    if devices.dtype == torch.float16 and not shared.opts.no_half_vae:
+        shared.log.warning("FLUX VAE doesn't support FP16! Enabling no-half-vae")
+        shared.opts.no_half_vae = True
     if "qint8" in checkpoint_info.name.lower() or "qint4" in checkpoint_info.name.lower():
         shared.log.debug(f'Loading FLUX: model="{checkpoint_info.name}" quant=True')
         from installer import install
@@ -44,6 +49,18 @@ def load_flux(checkpoint_info, diffusers_load_config):
         pipe = diffusers.FluxPipeline.from_pretrained(checkpoint_info.path, cache_dir=shared.opts.diffusers_dir, transformer=None, text_encoder_2=None, **diffusers_load_config)
         pipe.transformer = load_quanto_transformer(checkpoint_info.path)
         pipe.text_encoder_2 = load_quanto_text_encoder_2(checkpoint_info.path)
+        if pipe.transformer.dtype != devices.dtype:
+            try:
+                pipe.transformer = pipe.transformer.to(dtype=devices.dtype)
+            except Exception:
+                shared.log.error(f"FLUX: Failed to cast the transformer to {devices.dtype}! Set dtype to {pipe.transformer.dtype}")
+                raise
+        if pipe.text_encoder_2.dtype != devices.dtype:
+            try:
+                pipe.text_encoder_2 = pipe.text_encoder_2.to(dtype=devices.dtype)
+            except Exception:
+                shared.log.error(f"FLUX: Failed to cast the text encoder to {devices.dtype}! Set dtype to {pipe.text_encoder_2.dtype}")
+                raise
     else:
         pipe = diffusers.FluxPipeline.from_pretrained(checkpoint_info.path, cache_dir=shared.opts.diffusers_dir, **diffusers_load_config)
         shared.log.debug(f'Loading FLUX: model="{checkpoint_info.name}" quant=False')
