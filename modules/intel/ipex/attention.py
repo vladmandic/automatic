3e268e7cea290a2959728c5c201d39248252da0a
Disty0 <disty@disty.xyz>
2024-10-24 21:53:33 +0300

    Cleanup

--- modules/intel/ipex/attention.py
+++ modules/intel/ipex/attention.py
@@ -140,7 +140,7 @@ def scaled_dot_product_attention_32_bit(query, key, value, attn_mask=None, dropo
             if len(query.shape) == 4:
                 attn_mask = attn_mask.repeat((batch_size_attention // attn_mask.shape[0], query_tokens // attn_mask.shape[1], shape_three // attn_mask.shape[2], 1))
             else:
-               attn_mask = attn_mask.repeat((batch_size_attention // attn_mask.shape[0], query_tokens // attn_mask.shape[1], shape_three // attn_mask.shape[2]))
+                attn_mask = attn_mask.repeat((batch_size_attention // attn_mask.shape[0], query_tokens // attn_mask.shape[1], shape_three // attn_mask.shape[2]))
         for i in range(batch_size_attention // split_slice_size):
             start_idx = i * split_slice_size
             end_idx = (i + 1) * split_slice_size
