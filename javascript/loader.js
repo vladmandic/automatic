26b815941a74e1158676331c98ba7a20e3818b91
Vladimir Mandic <mandic00@live.com>
2024-11-01 15:36:10 -0400

    update backgrounds
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- javascript/loader.js
+++ javascript/loader.js
@@ -3,7 +3,7 @@ const appStartTime = performance.now();
 async function preloadImages() {
   const dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   const imagePromises = [];
-  const num = Math.floor(10 * Math.random());
+  const num = Math.floor(9.99 * Math.random());
   const imageUrls = [
     `file=html/logo-bg-${dark ? 'dark' : 'light'}.jpg`,
     `file=html/logo-bg-${num}.jpg`,
