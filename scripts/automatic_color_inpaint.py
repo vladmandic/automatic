9dc5125d97c1cfe0a18273bdd6119a2c31928cdf
P-Hellmann <phtijger@gmail.com>
2025-02-14 12:11:48 +0100

    added more control over the config, better default values

--- scripts/automatic_color_inpaint.py
+++ scripts/automatic_color_inpaint.py
@@ -9,7 +9,7 @@ Automatic Color Inpaint Script for SD.NEXT - SD & SDXL Support
 
 Author: Artheriax
 Credits: SD.NEXT team for script template
-Version: W.I.P
+Version: v1
 
 Contributions: A new script to automatically inpaint colors in images using Stable Diffusion or Stable Diffusion XL.
 """
@@ -31,7 +31,7 @@ img2img = True
 latent = True
 
 # pipeline args values are defined in ui method below
-params = ['color_to_mask', 'mask_tolerance']
+params = ['color_to_mask', 'mask_tolerance', 'mask_padding', 'mask_blur', 'inpaint_denoising_strength']
 
 
 ### Script definition
@@ -48,10 +48,12 @@ class Script(scripts.Script):
     # Define UI for pipeline
     def ui(self, _is_img2img):
         with gr.Row():
+            gr.HTML("&nbsp ACI: Automatic Color Inpaint<br>")
+        with gr.Row():
             color_picker = gr.ColorPicker(
                 label="Color to Mask",
                 value="#04F404",  # Default to green screen green
-                tooltip="Pick the color you want to mask and inpaint. Click on the color in the image to automatically select it.\n Advised to use images like green screens to get precise results."
+                info="Pick the color you want to mask and inpaint. Click on the color in the image to automatically select it.\n Advised to use images like green screens to get precise results."
             )
             tolerance_slider = gr.Slider(
                 minimum=0,
@@ -59,21 +61,45 @@ class Script(scripts.Script):
                 step=1,
                 value=25,
                 label="Color Tolerance",
-                tooltip="Adjust the tolerance to include similar colors in the mask.\nLower values mask only very similar colors.\nHigher values mask a wider range of similar colors.\n\nTweak both Tolerance and Denoising Strength in the Inpaint tab to achieve desired inpaint results."
+                info="Adjust the tolerance to include similar colors in the mask. Lower values = mask only very similar colors. Higher = values mask a wider range of similar colors."
             )
-        return [color_picker, tolerance_slider]
+            padding_slider = gr.Slider(
+                minimum=0,
+                maximum=256,
+                step=1,
+                value=2,
+                label="Mask Padding",
+                info="Adjust padding to apply a inside offset to the mask. (Recommended value = 2 to remove leftovers at edges)"
+            )
+            blur_slider = gr.Slider(
+                minimum=0,
+                maximum=64,
+                step=1,
+                value=0,
+                label="Mask Blur",
+                info="Adjust blur to apply a smooth transition between image and inpainted area. (Recommended value = 0 for sharpness)"
+            )
+            denoising_slider = gr.Slider(
+                minimum=0.01,
+                maximum=1,
+                step=0.01,
+                value=1,
+                label="Denoising Strength",
+                info="Change Denoising Strength to achieve desired inpaint amount."
+            )
+        return [color_picker, tolerance_slider, padding_slider, blur_slider, denoising_slider]
 
     # Run pipeline
     def run(self, p: processing.StableDiffusionProcessing, *args):  # pylint: disable=arguments-differ
         if shared.sd_model_type not in supported_models:
             shared.log.warning(f'MoD: class={shared.sd_model.__class__.__name__} model={shared.sd_model_type} required={supported_models}')
             return None
-        color_to_mask_hex, mask_tolerance = args
+        color_to_mask_hex, mask_tolerance, mask_padding, mask_blur, inpaint_denoising_strength = args
 
         # Convert hex color to RGB tuple (0-255)
         color_to_mask_rgb = tuple(int(color_to_mask_hex[i:i+2], 16) for i in (1, 3, 5))
 
-        shared.log.debug(f'{title}: Color to Mask={color_to_mask_rgb}, Tolerance={mask_tolerance}')
+        shared.log.debug(f'{title}: Color to Mask={color_to_mask_rgb}, Tolerance={mask_tolerance}, Padding={mask_padding}, Blur={mask_blur}, Denoising Strength={inpaint_denoising_strength}')
 
         # Create Color Mask using vectorized operations
         init_image = p.init_images[0].convert("RGB")
@@ -98,7 +124,9 @@ class Script(scripts.Script):
 
         # override inpaint parameters
         p.inpaint_full_res = True
-        p.task_args['mask_blur'] = 0
+        p.inpaint_full_res_padding = mask_padding
+        p.mask_blur = mask_blur
+        p.denoising_strength = inpaint_denoising_strength
 
         # Process the image using SD.Next’s inpainting
         processed: processing.Processed = processing.process_images(p)
